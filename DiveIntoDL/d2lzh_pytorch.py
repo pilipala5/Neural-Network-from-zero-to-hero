4cdc0e21e4bb4e3fc8d3ec0531544e65a0351cfd
pilipala5 <2021302131235@whu.edu.cn>
2024-07-24 21:31:33 +0800

    Until Resnet18 20240724

--- DiveIntoDL/d2lzh_pytorch.py
+++ DiveIntoDL/d2lzh_pytorch.py
@@ -142,3 +142,28 @@ def corr2d(X, K):
         for j in range(Y.shape[1]):
             Y[i, j] = (X[i: i + h, j: j + w] * K).sum()
     return Y
+
+
+class Residual(nn.Module):
+    def __init__(self, in_channels, out_channels,
+                 use_1x1conv=False, stride=1):
+        super(Residual, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels,
+                               kernel_size=3, padding=1, stride=stride)
+        self.conv2 = nn.Conv2d(out_channels, out_channels,
+                               kernel_size=3, padding=1)
+        if use_1x1conv:
+            self.conv3 = nn.Conv2d(in_channels, out_channels,
+                                   kernel_size=1, stride=stride)
+        else:
+            self.conv3 = None
+            self.bn1 = nn.BatchNorm2d(out_channels)
+            self.bn2 = nn.BatchNorm2d(out_channels)
+
+    def forward(self, X):
+        Y = F.relu(self.bn1(self.conv1(X)))
+
+        Y = self.bn2(self.conv2(Y))
+        if self.conv3:
+            X = self.conv3(X)
+        return F.relu(Y + X)
